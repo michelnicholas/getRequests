a6963212e754443d2c8a2e861123a3d9a3b0798b
Michel Nicholas <michelnicholas1993@gmail.com>
2024-12-18 18:07:09 -0500

    added number 1 and number 2

--- index.js
+++ index.js
@@ -8,8 +8,19 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+app.get("/random", (req, res) => {
+  //randomIndex is the variable that holds the random number to grab by the jokes length
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  // Jokes is the array kind of like json
+  res.json(jokes[randomIndex]);
+});
 
 //2. GET a specific joke
+app.get("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundJoke = jokes.find((joke) => joke.id === id);
+  res.json(foundJoke);
+});
 
 //3. GET a jokes by filtering on the joke type
 
